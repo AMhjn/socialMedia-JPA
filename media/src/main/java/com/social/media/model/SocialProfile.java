0caff12c1b0e9ed5e42adfd1d340cfa7b4e1661b
Abhir Mahajan <abhirmhjn@gmail.com>
2024-11-18 22:17:29 -0500

    JPA Practice using Social Media Scenario

---
+++ media/src/main/java/com/social/media/model/SocialProfile.java
@@ -0,0 +1,21 @@
+package com.social.media.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SocialProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "social_user")// for foreign key name
+    private SocialUser user;
+}
